ffd25acd0aaab9e0f82e1f2f26401dc943f0feaf
xiaoming2333ca <62623373+xiaoming2333ca@users.noreply.github.com>
2020-03-24 20:31:45 -0400

    Create Mailing Address.py

---
+++ Mailing Address.py
@@ -0,0 +1,15 @@
+first = input("Please enter your first name: ")
+last = input("please enter your last name: ")
+nation = input("please enter your nationality: ")
+state = input("please enter your state/province: ")
+city = input("your city: ")
+street = input("your street: ")
+number = input("your number: ")
+zip_number = input("zip number please: ")
+
+print(f'''
+{first} {last}
+{number} {street}
+{city} {state} {zip_number}
+{nation}
+''')
